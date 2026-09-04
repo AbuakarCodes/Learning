9e766944436b8548caa44bcc9a9be3c78bc087f0
Abubakar <abubakarzahid895@gmail.com>
2026-09-04 10:40:11 +0500

    Recursion Basics: iaArraySorted

--- src/Recursion/index.js
+++ src/Recursion/index.js
@@ -17,4 +17,14 @@ function sumNumbers(n) {
   if (n == 1) return 1
   return n + sumNumbers(n - 1)
 }
-console.log(sumNumbers(10));
+// console.log(sumNumbers(10));
+
+function isArraySorted(array, index = 0) {
+  if (index === array.length-1) return true 
+
+  if (array[index] > array[index + 1]) return false
+
+  return isArraySorted(array, index + 1)
+}
+
+console.log(isArraySorted([1, 2,3,4,5]));
