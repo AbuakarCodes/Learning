83f0d1b0ac036c2b4039b97078a14b36e1977cba
Abubakar <abubakarzahid895@gmail.com>
2026-09-15 18:11:13 +0500

    DP: solved minCostClimbStairs brute force solution

--- src/DP/index.js
+++ src/DP/index.js
@@ -1,21 +1,20 @@
 function timer(unit = "ms") {
-    const start = performance.now();
+  const start = performance.now()
 
-    return function endTimer() {
-        const end = performance.now();
-        const time = end - start;
+  return function endTimer() {
+    const end = performance.now()
+    const time = end - start
 
-        // console.log("Start:", start);
-        // console.log("End:", end);
+    // console.log("Start:", start);
+    // console.log("End:", end);
 
-        if (unit === "s") {
-            console.log("Time:", time / 1000, "seconds");
-        } else {
-            console.log("Time:", time, "ms");
-        }
-    };
+    if (unit === "s") {
+      console.log("Time:", time / 1000, "seconds")
+    } else {
+      console.log("Time:", time, "ms")
+    }
+  }
 }
-
 
 function editDistance(word1, word2) {
   // We are not actually modifying word1 or word2.
@@ -39,26 +38,43 @@ function editDistance(word1, word2) {
 
 // console.log(editDistance("horse", "ros"))
 
-
-
-
 function climbStairs(n) {
   let cache = []
-  
+
   function logic(n, step = 0) {
-    if (cache[step] != undefined) return  cache[step]
+    if (cache[step] != undefined) return cache[step]
     if (step === n) return 1
     if (step > n) return 0
 
-    let moveOne = logic(step+1,n)  
-    let moveTwo = logic(step+2,n) 
+    let moveOne = logic(n, step + 1)
+    let moveTwo = logic(n, step + 2)
 
-    cache[step] = moveOne + moveTwo 
+    cache[step] = moveOne + moveTwo
     return moveOne + moveTwo
   }
   return logic(n)
 }
+// console.log(climbStairs(5))
 
-const end = timer("ms");
-console.log( climbStairs(109),"gg")   
-end(); 
+
+
+function minCostClimbStairs(array) {
+  let globelCache = []
+  function minCostClimbStairs_logic(array, i = 0) {
+    if (globelCache[i] != undefined) return globelCache[i]
+
+    if (i > array.length - 1) return 0
+
+    let oneStep = minCostClimbStairs_logic(array, i + 1)
+    let twoStep = minCostClimbStairs_logic(array, i + 2)
+
+    globelCache[i] = Math.min(oneStep, twoStep) + array[i]
+
+    return globelCache[i]
+  }
+  minCostClimbStairs_logic(array)
+
+  return Math.min(globelCache[0], globelCache[1] )
+}
+
+console.log(minCostClimbStairs([1,100,1,1,1,100,1,1,100,1]))
